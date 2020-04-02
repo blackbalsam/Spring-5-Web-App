91dd4336860584e2ce1413b84b1ed6bd7c0fe9e8
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-02 15:19:35 +0300

    Adding author and book repositories

---
+++ src/main/java/gatis/example/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package gatis.example.spring5webapp.repositories;
+
+import gatis.example.spring5webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
---
+++ src/main/java/gatis/example/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package gatis.example.spring5webapp.repositories;
+
+import gatis.example.spring5webapp.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
